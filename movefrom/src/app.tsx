447870d854fe51dd31d69a545d27b0b20c786050
siwu-945 <sl3659@drexel.edu>
2023-12-01 22:10:06 -0500

    adding wallet

--- movefrom/src/app.tsx
+++ movefrom/src/app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MyForm from './form';
 import { AppContext } from './components/AppContext';
 import WalletConnect from './components/WalletConnect';
+import './style/app.css';
 
 function App() {
     return (
